cd3fa04999c71c85751d0958efe54fb5a79b8ece
eitchtee <flaips.2@gmail.com>
2019-07-09 14:54:17 -0300

    Introduz mudança ao código que permite que ele rode infinitamente, ao contrário da forma programática que ocorria anteriormente, respeitando SIGTERMs.

--- bitcoin_bipolar.py
+++ bitcoin_bipolar.py
@@ -1,36 +1,61 @@
-from twitter import twittar
-from bitcoin import valor_btc
-from money.money import Money
-from money.currency import Currency
 import pickle
+import signal
+import time
 from datetime import datetime
+
+from money.currency import Currency
+from money.money import Money
+
+from bitcoin import valor_btc
+from twitter import twittar
+
+
+class GracefulKiller:
+    kill_now = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        self.kill_now = True
 
 
 if __name__ == '__main__':
-    try:
-        with open('ultimo_valor.db', 'rb') as db:
-            ultimo_valor = pickle.load(db)
-    except FileNotFoundError:
-        print('Rodando pela primeira vez.')
-        valor_atual = valor_btc()
-        with open('ultimo_valor.db', 'wb') as db:
-            pickle.dump(valor_atual, db, protocol=pickle.HIGHEST_PROTOCOL)
-    else:
-        valor_atual = valor_btc()
-        diferenca = round(abs(valor_atual - ultimo_valor), 2)
-
-        if diferenca > 100:
-            valor_reais = Money(str(valor_atual), Currency.BRL).format('pt_BR')
-            hora = datetime.now().strftime('%H:%M')
-
-            if valor_atual > ultimo_valor:
-                msg = "Bitcoin subiu :) - {} às {}".format(valor_reais, hora)
-                twittar(msg)
-            elif ultimo_valor > valor_atual:
-                msg = "Bitcoin caiu :( - {} às {}".format(valor_reais, hora)
-                twittar(msg)
+    killer = GracefulKiller()
+    while not killer.kill_now:
+        try:
+            with open('ultimo_valor.db', 'rb') as db:
+                ultimo_valor = pickle.load(db)
+        except FileNotFoundError:
+            print('Rodando pela primeira vez.')
+            valor_atual = valor_btc()
+            with open('ultimo_valor.db', 'wb') as db:
+                pickle.dump(valor_atual, db, protocol=pickle.HIGHEST_PROTOCOL)
         else:
-            print('Diferença insignificante para ser postada.')
+            valor_atual = valor_btc()
+            diferenca = round(abs(valor_atual - ultimo_valor), 2)
 
-    with open('ultimo_valor.db', 'wb') as db:
-        pickle.dump(valor_atual, db, protocol=pickle.HIGHEST_PROTOCOL)
+            if diferenca > 800:
+                valor_reais = Money(str(valor_atual), Currency.BRL).format(
+                    'pt_BR')
+                hora = datetime.now().strftime('%H:%M')
+
+                if valor_atual > ultimo_valor:
+                    msg = "Bitcoin subiu :) - {} às {}".format(valor_reais,
+                                                               hora)
+                    twittar(msg)
+                    print(msg)
+                elif ultimo_valor > valor_atual:
+                    msg = "Bitcoin caiu :( - {} às {}".format(valor_reais, hora)
+                    twittar(msg)
+                    print(msg)
+                with open('ultimo_valor.db', 'wb') as db:
+                    pickle.dump(valor_atual, db,
+                                protocol=pickle.HIGHEST_PROTOCOL)
+            else:
+                print('Diferença insignificante para ser postada.')
+
+            time.sleep(300)
+
+    print("Parando execução.")
