ed98a01b292d8d3f4c0e3b8136998f5f05af1341
eitchtee <flaips.2@gmail.com>
2019-07-10 19:21:42 -0300

    Modificado o error handling para ser mais generalizado, evitando que o script pare aleatoriamente.

--- bitcoin.py
+++ bitcoin.py
@@ -1,5 +1,3 @@
-from json.decoder import JSONDecodeError
-
 import requests
 
 
@@ -7,10 +5,7 @@ def valor_btc(moeda: str = 'BRL'):
     api_link = "https://blockchain.info/ticker"
 
     request = requests.get(api_link)
-    try:
-        result_list = request.json()
-        valor = result_list[moeda]["last"]
-    except JSONDecodeError:
-        return None
+    result_list = request.json()
+    valor = result_list[moeda]["last"]
 
     return valor
--- bitcoin_bipolar.py
+++ bitcoin_bipolar.py
@@ -29,37 +29,43 @@ if __name__ == '__main__':
                 ultimo_valor = pickle.load(db)
         except FileNotFoundError:
             print('Rodando pela primeira vez.')
-            valor_atual = valor_btc()
+            try:
+                valor_atual = valor_btc()
+            except:
+                time.sleep(600)
+                continue
             with open('ultimo_valor.db', 'wb') as db:
                 pickle.dump(valor_atual, db, protocol=pickle.HIGHEST_PROTOCOL)
         else:
-            valor_atual = valor_btc()
-            # detecta problema na API, dorme por 10 minutos e continua o loop
-            if not valor_atual:
-                time.sleep(600)
+            try:
+                valor_atual = valor_btc()
+
+                diferenca = round(abs(valor_atual - ultimo_valor), 2)
+
+                if diferenca > 500:
+                    valor_reais = Money(str(valor_atual), Currency.BRL).format(
+                        'pt_BR')
+                    hora = datetime.now().strftime('%H:%M')
+
+                    if valor_atual > ultimo_valor:
+                        msg = "Bitcoin subiu :) - {} às {}".format(valor_reais,
+                                                                   hora)
+                        twittar(msg)
+                        print(msg)
+                    elif ultimo_valor > valor_atual:
+                        msg = "Bitcoin caiu :( - {} às {}".format(valor_reais,
+                                                                  hora)
+                        twittar(msg)
+                        print(msg)
+                    with open('ultimo_valor.db', 'wb') as db:
+                        pickle.dump(valor_atual, db,
+                                    protocol=pickle.HIGHEST_PROTOCOL)
+                else:
+                    print('Diferença insignificante para ser postada.')
+            except:
+                time.sleep(900)
                 continue
-            diferenca = round(abs(valor_atual - ultimo_valor), 2)
-
-            if diferenca > 500:
-                valor_reais = Money(str(valor_atual), Currency.BRL).format(
-                    'pt_BR')
-                hora = datetime.now().strftime('%H:%M')
-
-                if valor_atual > ultimo_valor:
-                    msg = "Bitcoin subiu :) - {} às {}".format(valor_reais,
-                                                               hora)
-                    twittar(msg)
-                    print(msg)
-                elif ultimo_valor > valor_atual:
-                    msg = "Bitcoin caiu :( - {} às {}".format(valor_reais, hora)
-                    twittar(msg)
-                    print(msg)
-                with open('ultimo_valor.db', 'wb') as db:
-                    pickle.dump(valor_atual, db,
-                                protocol=pickle.HIGHEST_PROTOCOL)
             else:
-                print('Diferença insignificante para ser postada.')
-
-            time.sleep(450)
+                time.sleep(450)
 
     print("Parando execução.")
