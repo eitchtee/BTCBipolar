89179791be172b39741284b15d1fa1d98f1a84a3
eitchtee <flaips.2@gmail.com>
2020-04-24 17:57:12 -0300

    Cria módulos separados para cada função principal.

--- bitcoin_bipolar.py
+++ bitcoin_bipolar.py
@@ -23,62 +23,65 @@ class GracefulKiller:
         self.kill_now = True
 
 
+def bitcoin_price_check():
+    try:
+        with open(db_path, 'rb') as db:
+            ultimo_valor = pickle.load(db)
+    except FileNotFoundError:
+        print('Rodando pela primeira vez.')
+        try:
+            valor_atual = valor_btc()
+        except:
+            return
+        with open(db_path, 'wb') as db:
+            pickle.dump(valor_atual, db, protocol=pickle.HIGHEST_PROTOCOL)
+    else:
+        try:
+            valor_atual = valor_btc()
+
+            diferenca = round(abs(valor_atual - ultimo_valor), 2)
+
+            if diferenca > 600:
+                valor_reais = Money(str(valor_atual), Currency.BRL). \
+                    format('pt_BR')
+                hora = datetime.now().strftime('%H:%M')
+
+                if valor_atual > ultimo_valor:
+                    msg = f"🟢 Bitcoin subiu :) - {valor_reais} às {hora}"
+                    try:
+                        twittar(msg)
+                    except:
+                        return
+                    print(msg)
+                elif ultimo_valor > valor_atual:
+                    msg = f"🔴 Bitcoin caiu :( - {valor_reais} às {hora}"
+                    try:
+                        twittar(msg)
+                    except:
+                        return
+                    print(msg)
+                with open(db_path, 'wb') as db:
+                    pickle.dump(valor_atual, db,
+                                protocol=pickle.HIGHEST_PROTOCOL)
+            else:
+                print(f'Diferença insignificante para ser postada. Ultimo '
+                      f'valor: {ultimo_valor} | Valor a'
+                      f'tual: {valor_atual} | Diferença: {diferenca}')
+        except:
+            return
+
+
+def halving_check():
+    pass
+
+
 if __name__ == '__main__':
     work_dir = os.path.dirname(os.path.realpath(__file__))
     db_path = os.path.normpath('{}/ultimo_valor.db'.format(work_dir))
 
     killer = GracefulKiller()
     while not killer.kill_now:
-        try:
-            with open(db_path, 'rb') as db:
-                ultimo_valor = pickle.load(db)
-        except FileNotFoundError:
-            print('Rodando pela primeira vez.')
-            try:
-                valor_atual = valor_btc()
-            except:
-                time.sleep(600)
-                continue
-            with open(db_path, 'wb') as db:
-                pickle.dump(valor_atual, db, protocol=pickle.HIGHEST_PROTOCOL)
-        else:
-            try:
-                valor_atual = valor_btc()
-
-                diferenca = round(abs(valor_atual - ultimo_valor), 2)
-
-                if diferenca > 600:
-                    valor_reais = Money(str(valor_atual), Currency.BRL). \
-                        format('pt_BR')
-                    hora = datetime.now().strftime('%H:%M')
-
-                    if valor_atual > ultimo_valor:
-                        msg = f"🟢 Bitcoin subiu :) - {valor_reais} às {hora}"
-                        try:
-                            twittar(msg)
-                        except:
-                            time.sleep(900)
-                            continue
-                        print(msg)
-                    elif ultimo_valor > valor_atual:
-                        msg = f"🔴 Bitcoin caiu :( - {valor_reais} às {hora}"
-                        try:
-                            twittar(msg)
-                        except:
-                            time.sleep(900)
-                            continue
-                        print(msg)
-                    with open(db_path, 'wb') as db:
-                        pickle.dump(valor_atual, db,
-                                    protocol=pickle.HIGHEST_PROTOCOL)
-                else:
-                    print(f'Diferença insignificante para ser postada. Ultimo '
-                          f'valor: {ultimo_valor} | Valor a'
-                          f'tual: {valor_atual} | Diferença: {diferenca}')
-            except:
-                time.sleep(900)
-                continue
-            else:
-                time.sleep(randint(300, 900))
+        bitcoin_price_check()
+        time.sleep(randint(300, 900))
 
     print("Parando execução.")
