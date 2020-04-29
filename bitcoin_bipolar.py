76075da28858ab87e008eea1ccce843afe58e5e4
eitchtee <flaips.2@gmail.com>
2020-04-29 08:08:26 -0300

    Adiciona valor em Dólar nos tweets. Extrai a checagem de diferença entre valores e se houve queda ou subida do valor para uma função única. Melhora o logging.

--- bitcoin_bipolar.py
+++ bitcoin_bipolar.py
@@ -3,6 +3,7 @@ import os
 import pickle
 import signal
 import time
+import traceback
 from datetime import datetime
 from random import randint
 
@@ -26,51 +27,81 @@ class GracefulKiller:
         self.kill_now = True
 
 
+def checar_diferenca(ultimo_valor, valor_atual):
+    valor_minimo = 600
+    diferenca = round(abs(valor_atual - ultimo_valor), 2)
+
+    return diferenca > valor_minimo, diferenca, valor_atual > ultimo_valor
+
+
 def bitcoin_price_check():
+    # Carrega o último valor verificado de uma db ou cria a database e
+    # aguarda a próxima execução
     try:
         with open(valor_db_path, 'rb') as db:
             ultimo_valor = pickle.load(db)
     except FileNotFoundError:
         print('Rodando pela primeira vez.')
         try:
-            valor_atual = valor_btc()
+            valor_atual_brl = valor_btc()
             with open(valor_db_path, 'wb') as db:
-                pickle.dump(valor_atual, db, protocol=pickle.HIGHEST_PROTOCOL)
+                pickle.dump(valor_atual_brl, db,
+                            protocol=pickle.HIGHEST_PROTOCOL)
         except:
+            traceback.print_exc()
             return
     else:
         try:
-            valor_atual = valor_btc()
+            valor_atual_brl, valor_atual_usd = valor_btc()
 
-            diferenca = round(abs(valor_atual - ultimo_valor), 2)
+            dif_check, dif_valor, subiu = checar_diferenca(ultimo_valor,
+                                                           valor_atual_brl)
 
-            if diferenca > 600:
-                valor_reais = Money(str(valor_atual), Currency.BRL). \
+            if dif_check:
+                valor_reais = Money(str(valor_atual_brl), Currency.BRL). \
+                    format('pt_BR')
+                valor_dolar = Money(str(valor_atual_usd), Currency.USD). \
                     format('pt_BR')
                 hora = datetime.now().strftime('%H:%M')
+                dia = datetime.now().strftime('%d/%m/%Y')
 
-                if valor_atual > ultimo_valor:
-                    msg = f"🟢 Bitcoin subiu :) - {valor_reais} às {hora}"
+                if subiu:
+                    msg = f"🟢 Bitcoin subiu :)\n" \
+                          f"🇧🇷 {valor_reais}\n" \
+                          f"🇺🇸 {valor_dolar}\n" \
+                          f"Em {dia} às {hora}."
                     try:
                         twittar(msg)
+                        print(f"🟢 Bitcoin subiu. "
+                              f'Último valor: {ultimo_valor} | '
+                              f'Valor atual: {valor_atual_brl} | '
+                              f'Diferença: {dif_valor}')
                     except:
+                        traceback.print_exc()
                         return
-                    print(msg)
-                elif ultimo_valor > valor_atual:
-                    msg = f"🔴 Bitcoin caiu :( - {valor_reais} às {hora}"
+                else:
+                    msg = f"🔴 Bitcoin caiu :(\n" \
+                          f"🇧🇷 {valor_reais}\n" \
+                          f"🇺🇸 {valor_dolar}\n" \
+                          f"Em {dia} às {hora}."
                     try:
                         twittar(msg)
+                        print(f"🔴 Bitcoin caiu. "
+                              f'Último valor: {ultimo_valor} | '
+                              f'Valor atual: {valor_atual_brl} | '
+                              f'Diferença: {dif_valor}')
                     except:
+                        traceback.print_exc()
                         return
-                    print(msg)
                 with open(valor_db_path, 'wb') as db:
-                    pickle.dump(valor_atual, db,
+                    pickle.dump(valor_atual_brl, db,
                                 protocol=pickle.HIGHEST_PROTOCOL)
             else:
-                print(f'Diferença insignificante para ser postada. Ultimo '
+                print(f'Diferença insignificante para ser postada. Último '
                       f'valor: {ultimo_valor} | Valor a'
-                      f'tual: {valor_atual} | Diferença: {diferenca}')
+                      f'tual: {valor_atual_brl} | Diferença: {dif_valor}')
         except:
+            traceback.print_exc()
             return
 
 
@@ -85,6 +116,7 @@ def halving_check():
             with open(bloco_db_path, 'wb') as db:
                 pickle.dump(cur_bloco, db, protocol=pickle.HIGHEST_PROTOCOL)
         except:
+            traceback.print_exc()
             return
 
     halving_check_num = 0
@@ -99,6 +131,7 @@ def halving_check():
         try:
             block_creation_stamp = block_date(halving_check_num)
         except:
+            traceback.print_exc()
             print('Não foi possível acessar data do bloco. Tentando na '
                   'próxima ronda.')
             return
@@ -113,7 +146,11 @@ def halving_check():
               f'às {block_hour} de {block_day}.'
         try:
             twittar(msg)
+            print(f'⚠ O Bitcoin Halving de {ano} aconteceu! '
+                  f'O bloco {halving_check_num:n} foi criado '
+                  f'às {block_hour} de {block_day}.')
         except:
+            traceback.print_exc()
             return
     else:
         print(f"Halving ainda não aconteceu. Bloco atual: {cur_bloco} | "
@@ -134,7 +171,7 @@ if __name__ == '__main__':
     while not killer.kill_now:
         bitcoin_price_check()
         halving_check()
-        print()
+        print("---")  # Separa os logs de cada execução.
         time.sleep(randint(300, 900))
 
     print("Parando execução.")
